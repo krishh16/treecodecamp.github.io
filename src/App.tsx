c65aa5889b4d68ae1750e42f98ca565ecb34971a
zeref31 <zeref18.drgnl@gmail.com>
2023-06-18 16:20:41 +0530

    made the landing page for treecodecamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
+import {  
+  BrowserRouter as Router,
+  Routes,
+  Route
+
+} from 'react-router-dom'
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
