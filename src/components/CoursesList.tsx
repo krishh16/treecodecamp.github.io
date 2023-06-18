a5a09e6e525bd3239b5b1a87947f8708f5ffe982
zeref31 <zeref18.drgnl@gmail.com>
2023-06-18 17:15:16 +0530

    added the ui for courses

--- src/components/CoursesList.tsx
+++ src/components/CoursesList.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-export default function Courses() {
+export default function CoursesList() {
     return (
         <div>
-            <h2 className='font-extrabold m-5' style={{fontSize: '30px'}}>Courses</h2>
+            <h2 className='font-extrabold m-5 text-center text-white' style={{fontSize: '30px'}}>Courses</h2>
             <div className='flex items-center justify-center '>
                 <Link to="/" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Javascript Course on Data Structure and Algorithms</h5>
