96ab85f69ef4fc01112b08e2cc3129c92d706a01
zeref31 <zeref18.drgnl@gmail.com>
2023-06-21 21:59:39 +0530

    Co-authored-by: krishna <127577065+krishh16@users.noreply.github.com>

--- src/components/pages/Terminal/Terminal.jsx
+++ src/components/pages/Terminal/Terminal.jsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-import { okaidia } from '@uiw/codemirror-theme-okaidia';
+import CodeMirror from "@uiw/react-codemirror";
+import { createTheme } from "@uiw/codemirror-themes";
+import { javascript } from "@codemirror/lang-javascript";
+import { tags as t } from "@lezer/highlight";
 
+import React from "react";
+const myTheme = createTheme({
+  theme: "light",
+  settings: {
+    background: "#ffffff",
+    foreground: "#000",
+    caret: "red",
+    selection: "#8a91991a",
+    selectionMatch: "red",
+    lineHighlight: "#8a91991a",
+    gutterBackground: "#fff",
+    gutterForeground: "#D9DEEB"
+  },
+  styles: [
+    { tag: t.comment, color: "#919EAD" },
+    { tag: t.variableName, color: "#6D6BDE" },
+    { tag: t.quote, color: "#000" },
+    { tag: t.moduleKeyword, color: "red" },
+    { tag: [t.string, t.special(t.brace)], color: "#56BA90" },
+    { tag: t.number, color: "#5c6166" },
+    { tag: t.bool, color: "#5c6166" },
+    { tag: t.null, color: "#5c6166" },
+    { tag: t.keyword, color: "#5c6166" },
+    { tag: t.operator, color: "#5c6166" },
+    { tag: t.className, color: "#5c6166" },
+    { tag: t.definition(t.typeName), color: "#5c6166" },
+    { tag: t.typeName, color: "red" },
+    { tag: t.angleBracket, color: "#000" },
+    { tag: t.tagName, color: "#fff" },
+    { tag: t.attributeName, color: "#41ABE1" },
+    { tag: t.propertyName, color: "#56BA90" },
+    { tag: t.annotation, color: "red" }
+  ]
+});
+const extensions = [javascript({ jsx: true })];
 
-export default function Terminal() {
-  const onChange = React.useCallback((value, viewUpdate) => {
-    console.log('value:', value);
-  }, []);
+export default function App() {
+  const onChange = React.useCallback((value, viewUpdate) => {}, []);
   return (
-    <div className='bg-white border-gray-200 dark:bg-gray-900' style={{width:'100vw', height:'100vh'}}>
     <CodeMirror
-      value="//add your code here"
-      height="100px"
-      width='50vw'
-      extensions={[javascript({ jsx: true })]}
+      style={{ border: "2px solid red", borderRadius: "4px" }}
+      value="console.log('hello world!');"
+      height="200px"
+      theme={myTheme}
+      extensions={extensions}
       onChange={onChange}
-      theme='dark'
-      basicSetup={true}
     />
-    </div>
   );
 }
